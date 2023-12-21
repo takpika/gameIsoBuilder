cbca8d4955f1962e77281bd60515becd141092c5
takpika <takpika@mail.takpika.jp>
2023-12-21 15:31:55 +0800

    add stop command

--- main.py
+++ main.py
@@ -113,7 +113,7 @@ class LinuxWineIsoBuilder:
         ### Install Softwares
         self.runCmd('apt install -y xserver-xorg-core xserver-xorg-video-fbdev x11-xserver-utils xinit xinput --no-install-recommends --no-install-suggests')
         self.runCmd('apt install -y xserver-xorg-input-wacom xserver-xorg-input-mouse xserver-xorg-input-kbd xserver-xorg-input-libinput --no-install-recommends --no-install-suggests')
-        self.runCmd('apt install -y pulseaudio ca-certificates eject --no-install-recommends --no-install-suggests')
+        self.runCmd('apt install -y pulseaudio ca-certificates eject psmisc --no-install-recommends --no-install-suggests')
         ### Setup Xinit
         self.copyConfig("rootdir/usr/local/bin/set-resolution.sh", replace={"{{RESOLUTION}}": self.screenResolution})
         self.runCmd('chmod +x /usr/local/bin/set-resolution.sh')
